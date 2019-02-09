2c978d9ba112270d097521dde8030bcc342500c1
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-09 11:11:55 -0200

    Initial commit

---
+++ app/src/main/java/com/rafael/pessoa/githubaac/data/local/converter/DateConverter.kt
@@ -0,0 +1,18 @@
+package com.rafael.pessoa.githubaac.data.local.converter
+
+import android.arch.persistence.room.TypeConverter
+import java.util.*
+
+object DataConverter{
+
+    @TypeConverter @JvmStatic
+    fun toDate(timeStamp: Long?): Date? {
+        return if(timeStamp == null) null else Date(timeStamp)
+    }
+
+    @TypeConverter @JvmStatic
+    fun toTimeStamp(date: Date?): Long? {
+        return date?.time
+    }
+
+}
---
+++ app/src/main/java/com/rafael/pessoa/githubaac/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rafael.pessoa.githubaac.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.rafael.pessoa.githubaac.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
